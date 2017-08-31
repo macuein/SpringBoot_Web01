54a57a28d72f291521da25d35e18c9969db5d43d
Manuel Izurieta <manuel@aaa.aaa>
2017-08-30 23:59:23 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpringBoot_Web01
---
+++ src/main/resources/data/hsqldb/script_hsqldb_dml.sql
@@ -0,0 +1,5 @@
+insert into employee (emp_name, emp_designation, emp_salary) values ('aaaa','bbbb',1000);
+insert into employee (emp_name, emp_designation, emp_salary) values ('cccc','dddd',2000);
+insert into employee (emp_name, emp_designation, emp_salary) values ('eeee','ffff',3000);
+
+
